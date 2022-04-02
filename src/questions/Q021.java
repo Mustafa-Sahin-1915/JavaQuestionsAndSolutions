1466fba0cb1e180a78ff4b0c01644520bbfe27d0
M Sah <msahin@comu.edu.tr>
2022-04-02 13:16:12 +0300

    ok

---
+++ src/questions/Q021.java
@@ -0,0 +1,98 @@
+package questions;
+/*
+Amazon interview question
+string permutation
+write a method that takes string and two integer index value as a parameter
+then print out on the console all permutations of that string.
+these permutations must be done on bounded part of the string which is from startindex to endindex.
+Both indexes are inclusive.
+
+permute("ABCD",1,2);  => ABCD, ACBD
+
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Q021 {
+    public static void main(String[] args) {
+        String str = "abcdefg";
+        permute(str,2,4);
+    }
+    public static void permute(String s, int startindex,int endindex){
+        List<String> listOfPermutations;
+        String subPart = s.substring(startindex,endindex+1);
+        listOfPermutations = permutationGenerate(subPart);
+        String newS="";
+        for (String w:listOfPermutations){
+            newS = s.replace(subPart,w);
+            System.out.println(newS);
+        }
+    }
+    public static List<String> permutationGenerate(String s){
+        List<String> listOfPermutations = new ArrayList<>();
+        int n = s.length();
+        char chars[] = s.toCharArray();
+        int counter[] = new int[n];
+        boolean isCounterZero = false;
+        boolean isCounterDistinct = false;
+        while(!isCounterZero){
+            while(true){
+                counter = click(counter);
+                isCounterDistinct = isDistinct(counter);
+                isCounterZero = isZeroAgain(counter);
+                if (isCounterDistinct || isCounterZero) break;
+            }
+            if (!isCounterZero){
+                listOfPermutations.add(printOutPermutation(s,counter));
+            }
+        }
+        return listOfPermutations;
+    }
+
+    public static int[] click(int[] counter){
+        int a;
+        int b=1;
+        for (int i = counter.length-1; i >=0 ; i--) {
+            a=(counter[i]+b)%counter.length;
+            counter[i]= a;
+            if (a==0){
+                b=1;
+            }else{
+                b=0;break;
+            }
+        }
+        return counter;
+    }
+    public static boolean isDistinct(int[] counter){
+        boolean res = true;
+        int leftindex =-1;
+        int rightindex =-1;
+        for (int i = 0; i < counter.length; i++) {
+            leftindex=-1;
+            rightindex=-1;
+            for (int j = 0; j < counter.length; j++) {
+                if (counter[i]==counter[j]) leftindex=j;
+            }
+            for (int k = counter.length-1; k >=0;k--) {
+                if (counter[i]==counter[k]) rightindex=k;
+            }
+            if (leftindex!=rightindex) return false;
+        }
+        return true;
+    }
+    public static boolean isZeroAgain(int[] counter){
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i]!=0) return false;
+        }
+        return true;
+    }
+    public static String printOutPermutation(String s, int[] counter){
+        StringBuilder sb = new StringBuilder();
+        for (int w:counter) {
+            sb.append(s.charAt(w));
+        }
+        return sb.toString();
+    }
+}
